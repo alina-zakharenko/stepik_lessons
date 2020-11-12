59574a28c707e9a449adcb0810ae1cec2c557014
azakhare <alina.zakharenko@t-systems.com>
2020-11-12 11:02:37 +0300

    commit and push corrected module_2/test_registration.py via IDE

--- module_2/test_registration.py
+++ module_2/test_registration.py
@@ -2,19 +2,25 @@ from selenium import webdriver
 import time
 
 try:
-#    link = "http://suninjuly.github.io/registration1.html"
-    link = "http://suninjuly.github.io/registration2.html"
+    link = "http://suninjuly.github.io/registration1.html"
+    #link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element_by_xpath("//input[contains(@placeholder, \"Input your first name\")]")
+    input1 = browser.find_element_by_xpath(
+    "//div[contains(@class, 'first_block')]/div[contains(@class,'form-group first_class')]/input")
+    # ("//input[contains(@placeholder, \"Input your first name\")]")
     input1.send_keys("Ivan")
     input2 = browser.find_element_by_xpath("//div[contains(@class,'container')]//div[contains(@class, 'first_block')]/div[contains(@class,'form-group second_class')]/input")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_xpath("//input[contains(@placeholder, \"Input your email\")]")
+    input3 = browser.find_element_by_xpath("//input[contains(@placeholder, 'Input your email')]")
+    # ("//div[contains(@class, 'first_block')]/div[contains(@class,'form-group third_class')]/input")
+
     input3.send_keys("Smolensk@ru")
 
+    # Проверяем, что форма заполнилась значениями
+    time.sleep(5)
 
     # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
@@ -32,8 +38,13 @@ try:
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
 
+
 finally:
+
     # ожидание чтобы визуально оценить результаты прохождения скрипта
+
     time.sleep(10)
+
     # закрываем браузер после всех манипуляций
+
     browser.quit()
