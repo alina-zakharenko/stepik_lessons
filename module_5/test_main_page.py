6baf7228b96a7cc367783aa991dc7d4cb0faa0b4
azakhare <alina.zakharenko@t-systems.com>
2020-12-08 18:22:14 +0300

    commit module5 first tests with page object pattern

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
 
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
+#link = "http://selenium1py.pythonanywhere.com/de/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+link2= "http://selenium1py.pythonanywhere.com/de/accounts/login/"
 
 
 class TestMainPage:
@@ -15,3 +18,20 @@ class TestMainPage:
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
+
+    def test_should_be_login_url(self, browser):
+        page = LoginPage(browser, link2)
+        page.open()
+        page.should_be_login_url()
+
+    def test_should_be_login_form(self, browser):
+        page = LoginPage(browser, link2)
+        page.open()
+        page.should_be_login_form()
+
+    def test_should_be_register_form(self, browser):
+        page = LoginPage(browser, link2)
+        page.open()
+        page.should_be_register_form()
+
+
