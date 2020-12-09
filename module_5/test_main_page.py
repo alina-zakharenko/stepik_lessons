3e1e06f6a594776dbd9f9d565b3e725f771080a3
azakhare <alina.zakharenko@t-systems.com>
2020-12-09 11:01:16 +0300

    commit module5 switch between pages - corrected test with page object pattern

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -3,38 +3,37 @@ from selenium.webdriver.common.by import By
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
-#link = "http://selenium1py.pythonanywhere.com/de/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
-link2= "http://selenium1py.pythonanywhere.com/de/accounts/login/"
-
-
 class TestMainPage:
 
-    def test_guest_can_go_to_login_page(browser):
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
         page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логинa
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
+        login_page.should_be_login_url()
+        login_page.should_be_register_form()
+        login_page.should_be_login_form()
 
-    def test_guest_should_see_login_link(self, browser):
-        page = MainPage(browser, link)
-        page.open()
-        page.should_be_login_link()
 
-    def test_should_be_login_url(self, browser):
-        page = LoginPage(browser, link2)
-        page.open()
-        page.should_be_login_url()
 
-    def test_should_be_login_form(self, browser):
-        page = LoginPage(browser, link2)
-        page.open()
-        page.should_be_login_form()
 
-    def test_should_be_register_form(self, browser):
-        page = LoginPage(browser, link2)
-        page.open()
-        page.should_be_register_form()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
