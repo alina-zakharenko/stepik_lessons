f79fb0b8c87f73743b177056949db6a3c01a8ea5
azakhare <alina.zakharenko@t-systems.com>
2020-12-25 11:06:39 +0300

    commit and push final project after review

--- final/test_product_page.py
+++ final/test_product_page.py
@@ -16,11 +16,13 @@ class TestProductPage:
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                              pytest.param(
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  marks=pytest.mark.xfail),
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
                               ])
-    @pytest.mark.xfail(reason="Задание: независимость контента, ищем баг")
+    # @pytest.mark.xfail(reason="Задание: независимость контента, ищем баг")
     def test_guest_can_add_product_to_basket(self, browser, link):
         # Data
         product_name = "Coders at Work"
